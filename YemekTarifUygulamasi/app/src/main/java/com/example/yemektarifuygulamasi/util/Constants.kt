27c334ad468e40ec835d01085639dcaeffe3c5ca
ServetErdoğan <136233077+ServetErdogan09@users.noreply.github.com>
2024-10-18 11:37:48 +0300

    Update Constants.kt

--- YemekTarifUygulamasi/app/src/main/java/com/example/yemektarifuygulamasi/util/Constants.kt
+++ YemekTarifUygulamasi/app/src/main/java/com/example/yemektarifuygulamasi/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.yemektarifuygulamasi.util
 
 object Constants {
 
-    val API_KEY = "6525a61e86c34b70b074ffaa42e00fb5"
+    val API_KEY = "YOUR_API_KEY"
     val BASE_URL = "https://api.spoonacular.com/"
 
 }
